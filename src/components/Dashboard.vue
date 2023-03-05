5866ce6082fbcf1cc58d9a4fe22e537a3f9e72bc
Colin Skinner <colinskinner4@gmail.com>
2023-03-04 18:33:13 -0600

    Roughed in Dashboard

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,33 @@
+<script>
+
+</script>
+
+<template>
+    <main>
+        <img alt="Vue logo" class="logo" src="src/assets/logo.svg" width="125" height="125" />
+
+        <div id="climber-rating">
+            <h1>Color Rating</h1>
+            <h1>Number Rating</h1>
+        </div>
+
+        <div id="swipe-prompt">
+            <p>Swipe up to view timeline</p>
+            <p>&#x2193</p>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+main {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+}
+
+h1, p {
+    text-align: center;
+}
+</style>
